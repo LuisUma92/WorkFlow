62d60f753c7d5e8ccd531111c4d28f3ffab9678c
Luis Fernando Umana Castro <luis.umanacastro@ucr.ac.cr>
2025-10-08 16:08:19 -0600

    CreTE - update - v2.0

--- src/lectkit/crete.py
+++ src/lectkit/crete.py
@@ -1,3 +1,4 @@
+from re import M
 import click
 import subprocess
 from pathlib import Path
@@ -13,12 +14,19 @@ def cli():
 
 # loading User config folder
 user_dir = Path(user_config_dir("crete", "LuisUmana"))
+template_dir = Path(user_config_dir("mytex/templates", "LuisUmana"))
 if not user_dir.is_dir():
     user_dir.mkdir()
 book_list = user_dir / "books.json"
-
+HEADER = ""
+BOTTOM = ""
+MAIN_TEMPLATE = "00AA.tex"
+EXERCISE_TEMPLATE = "TNNE000.tex"
+VERBOSE = 1
 
 # loading current root files
+
+
 @cli.command()
 def init_books():
     if not book_list.is_file():
@@ -31,9 +39,11 @@ def init_books():
 def add_book(bookinfo):
     with open(bookinfo, "r") as file:
         contents = json.load(file)
+    log(f"{contents['name']}: readed", 3)
     with open(book_list, "r") as file:
         old_contents = json.load(file)
     old_contents["library"].append(contents)
+    log(f"{contents['name']}: added to book_lias", 3)
     with open(book_list, "w") as file:
         json.dump(old_contents, file)
     return contents
@@ -48,8 +58,8 @@ def get_book(book):
     return {}
 
 
-def chapter_path(name, code, ch):
-    return f"er-{code}-{name}/530-{name.capitalize()}-C{ch:02d}"
+def chapter_path(name, code, edition, ch):
+    return f"{code}_{name.capitalize()}_{edition}/C{ch:02d}"
 
 
 def file_upto_section(name, chNum, secNum):
@@ -57,163 +67,128 @@ def file_upto_section(name, chNum, secNum):
 
 
 def current_file(path, fout, idx):
-    thisF = path / "".join([fout, "P{:03d}.tex".format(idx)])
-    return thisF
+    return f"{path}/{fout}P{idx:03d}.tex"
 
 
-HEADER = """\\documentclass[12pt,
-  notitlepage,
-  % openany,
-  twoside,
-  % twocolumn,
-  ]{article}
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%==<Definición de comandos propios>==================
-\\newcommand{\\AAfolder}{/home/luis/.config/mytex}
-\\usepackage{\\AAfolder/sty/SetFormat}
-\\newcommand{\\AMfolder}{/home/luis/Documents/01-U/00-00AA-Apuntes}
-\\usepackage{\\AAfolder/sty/ColorsLight}
-\\usepackage{\\AAfolder/sty/SetSymbols}
-\\addbibresource{\\AAfolder/bib/Biblioteca.bib}
-\\usepackage{\\AAfolder/sty/HW-header}
-\\setbool{solutions}{true}
-\\newboolean{MC}
-\\setbool{MC}{true}
-\\graphicspath{{\\AMfolder/img/530_S439fi14-Sears}}
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%==<Inicia documento>================================
-\\begin{document}
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% \\maketitle
-% \\tableofcontents
-% \\listoffigures
-% \\listoftables
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-"""
-
-BOTTOM = """
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%==<Bibliografía>====================================
-%		Para la bibliografía se usa el gestor de
-%	referencias bibtex. Toda la información de las
-%	referencias se pone en un archivo aparte con la
-%	terminación .bib y se agrega aquí
-%
-% \\printbibliography
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%==<Apéndice>========================================
-%		Se puede crear apéndices que luego se pueden
-%	referenciar con la etiqueta
-% \\appendix
-% \\section{Figuras \\label{app:fig}}
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%==<Index>===========================================
-% \\printindex
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%==<Fin del documento>===============================
-\\end{document}
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"""
-
-
-def add_inputs(name, code, chNum, secNum, start, end, main_path="\\AMfolder/"):
-    CHPATH = chapter_path(name, code, chNum)
-    path = Path(main_path + CHPATH)
+def add_inputs(
+    name,
+    code,
+    edition,
+    chNum,
+    secNum,
+    start,
+    end,
+    main_path="\\FisicaDir/",
+):
+    chapter_relative_path = chapter_path(name, code, edition, chNum)
+    path = f"{main_path}/{chapter_relative_path}"
     fout = file_upto_section(name, chNum, secNum)
     text = ""
     for idx in range(start, end + 1):
-        thisF = path / "".join([fout, "P{:03d}.tex".format(idx)])
+        thisF = current_file(path, fout, idx)
         text += "\t\\input{" + f"{thisF}" + "}\n"
     return text
 
 
-def exercises_file_content(ch, idx, book):
-    msn = "\\ifthenelse{\\boolean{MC}}{\n\\exa["
-    msn += str(ch) + "]{" + str(idx) + "} %\\cite{" + str(book)
-    msn += "}\n}{\\exa{}}\n.\n\n\\begin{enumerate}[a)]\n\\item ."
-    msn += "\n\\end{enumerate}\n\n\\ifthenelse{\\boolean{solutions}}{"
-    msn += "\n  \\paragraph{Solución:}\n  \\begin{enumerate}[a)]"
-    msn += "\n    \\item .\n  Datos:\n  \\[\\begin{array}{l}"
-    msn += "\n  \\end{array}\\] \n  \\end{enumerate} \n}{}"
+def exercises_file_content(
+    ch,
+    idx,
+    book,
+    template_file_name=EXERCISE_TEMPLATE,
+):
+    with open(template_dir / template_file_name, "r") as f:
+        exe_file = f.readlines()
+    reference_info = (
+        f"  \\exe[{ch}]" + "{" + str(idx) + "} %\\cite{" + str(book) + "}\n"
+    )
+    exe_file[1] = reference_info
+    msn = "".join(exe_file)
     return msn
 
 
-# @cli.command()
-# @click.argument(
-#     'start',
-#     default=1,
-#     type=int
-# )
-# @click.argument(
-#     'end',
-#     default=1,
-#     type=int
-# )
-# @click.argument(
-#     'ch',
-#     default=1,
-#     type=int
-# )
-# @click.argument(
-#     'sec',
-#     default='01',
-#     type=str
-# )
-# @click.argument(
-#     'name',
-#     type=str
-# )
-# @click.argument(
-#     'code',
-#     type=str
-# )
-def create_solution_file(start, end, ch, sec, name, code):
-    CHPATH = chapter_path(name, code, ch)
-    path = Path(f"./{CHPATH}").absolute()
-    # log(f'''
-    # Se está trabajando en:
-    # {path}''', verbose)
+def init_head_bottom(template_file_name=MAIN_TEMPLATE):
+    with open(template_dir / template_file_name, "r") as f:
+        template_file = f.readlines()
+    reading_head = True
+    for line in template_file:
+        if reading_head:
+            global HEADER
+            HEADER += line
+            if line == "> contents":
+                reading_head = False
+        else:
+            global BOTTOM
+            BOTTOM += line
+
+
+def create_solution_file(start, end, ch, sec, name, code, edition, output):
+    chapter_relative_dir = chapter_path(name, code, edition, ch)
+    path = output / chapter_relative_dir
     if not path.exists():
         os.makedirs(path)
     fout = file_upto_section(name, ch, sec)
+    text = ""
     for idx in range(start, end + 1):
         thisF = current_file(path, fout, idx)
-        subprocess.run(["touch", thisF])
         msn = exercises_file_content(ch, idx, name)
-        lines = msn.split("\n")
-        for line in lines:
-            os.system('echo "' + line + '" >> ' + str(thisF))
-    pass
+        include_path = current_file(chapter_relative_dir, fout, idx)
+        text += "\t\\input{" + include_path + "}\n"
+        with open(path / thisF, "w") as file:
+            file.write(msn)
+    return text
 
 
-def create_book_solutions_files(book):
+def create_book_solutions_files(book, output_dir):
     main_text = ""
     for sec in book["distro"]:
-        create_solution_file(sec[2], sec[3], sec[0], sec[1], book["name"], book["code"])
-        main_text += add_inputs(
-            book["name"], book["code"], sec[0], sec[1], sec[2], sec[3]
+        main_text += create_solution_file(
+            sec[2],
+            sec[3],
+            sec[0],
+            sec[1],
+            book["name"],
+            book["code"],
+            book["edition"],
+            output_dir,
         )
-    soluPath = chapter_path(book["name"], book["code"], "00")
-    soluPath = soluPath
-    with open(f"ER-{book['code']}.tex", "w") as file:
+    init_head_bottom()
+    outputfile_name = f"ER-{book['code']}-{book['name']}-{book['edition']}.tex"
+    with open(output_dir / outputfile_name, "w") as file:
         file.write(HEADER)
         file.write(main_text)
         file.write(BOTTOM)
+    log(f"Main wrote to: {outputfile_name}", 3)
 
 
 @cli.command()
 @click.option("--file", default=book_list)
 @click.option("--name", default="none")
-def init(file, name):
+@click.option("--output", default=".")
+@click.option("--verbose", default=1)
+def init(file, name, output, verbose):
+    global VERBOSE
+    VERBOSE = verbose
     book = get_book(name)
     if not book:
-        print("Requested book is not in bib")
+        log(f"Requested book: {name}\n  is not in bib", 0)
         book = add_book(file)
         name = book["name"]
-    create_book_solutions_files(book)
+
+    output_dir = Path(output).expanduser()
+    output_dir = output_dir.absolute()
+    msn = f"""Configurarion:
+    Exercise template: {EXERCISE_TEMPLATE}
+    Main template: {MAIN_TEMPLATE}
+    Output directory: {output_dir}
+    Book to process: {book["code"]}-{book["name"]}-{book["edition"]}
+    """
+    log(msn, 3)
+    create_book_solutions_files(book, output_dir)
+    log("----- Done ------", 0)
 
 
 def log(mns, verbose=2):
-    if verbose > 1:
+    if verbose < VERBOSE:
         print(mns)
 
 
