83f559ac90a52b187f5ecb45ca217e6480bd6928
Luis Fernando Umaña Castro <luis.umanacastro@ucr.ac.cr>
2026-06-04 09:52:48 -0600

    Updating exercises templates

--- nvim-plugin/lua/workflow/snippets/tex.lua
+++ nvim-plugin/lua/workflow/snippets/tex.lua
@@ -15,9 +15,10 @@ local function tpl(filename)
 end
 
 return {
-	s({ trig = "tpl.partial",  name = "Partial exam (PN-YYYY-IIIC)" },  { tpl("PN-YYYY-IIIC.tex") }),
-	s({ trig = "tpl.proposal", name = "Partial proposal" },              { tpl("PartialPropousal.tex") }),
-	s({ trig = "tpl.main",     name = "Main document (00AA)" },          { tpl("00AA.tex") }),
-	s({ trig = "tpl.bookex",   name = "Book exercise (C00S00P000)" },    { tpl("book-C00S00P000.tex") }),
-	s({ trig = "tpl.lect",     name = "Lecture article ficha" },         { tpl("lect.tex") }),
+	s({ trig = "tpl.partial", name = "Partial exam (PN-YYYY-IIIC)" }, { tpl("PN-YYYY-IIIC.tex") }),
+	s({ trig = "tpl.proposal", name = "Partial proposal" }, { tpl("PartialPropousal.tex") }),
+	s({ trig = "tpl.main", name = "Main document (00AA)" }, { tpl("00AA.tex") }),
+	s({ trig = "tpl.exEssey", name = "Book exercise (C00S00P000)" }, { tpl("book-TDEC00S00P000.tex") }),
+	s({ trig = "tpl.exMultichoice", name = "Book exercise (C00S00P000)" }, { tpl("book-TSUC00S00P000.tex") }),
+	s({ trig = "tpl.lect", name = "Lecture article ficha" }, { tpl("lect.tex") }),
 }
